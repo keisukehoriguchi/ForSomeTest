b014291cc9d58f272d85058b4be3de92d2fe96f6
hor1guch1ke1suke <hor1guch1ke1suke@yahoo.co.jp>
2021-05-08 11:21:44 +0900

    プロジェクト作成

---
+++ ForSomeTest/ForSomeTest/ForSomeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForSomeTestApp.swift
+//  ForSomeTest
+//
+//  Created by Keisuke Horiguchi on 2021/05/08.
+//
+
+import SwiftUI
+
+@main
+struct ForSomeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
